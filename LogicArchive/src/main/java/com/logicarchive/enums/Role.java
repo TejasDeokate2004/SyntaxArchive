5000a9d82c53622ae564c1f7771fbad8a94efc5c
Tejas Deokate <tejasdeokate45@gmail.com>
2026-08-17 21:26:26 +0530

    Delete LogicArchive directory

--- LogicArchive/src/main/java/com/logicarchive/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.logicarchive.enums;
-
-public enum Role {
-    STUDENT,
-    ADMIN
-}
--- LogicArchive/src/main/java/com/logicarchive/enums/Visibility.java
+++
@@ -1,6 +0,0 @@
-package com.logicarchive.enums;
-
-public enum Visibility {
-    PUBLIC,
-    PRIVATE
-}
